4747f52cd9a511d95d3a068da765a252d8a7290e
Sone <sthaya@gmail.com>
2020-09-12 16:46:40 -0700

    created about page, added icons and content, grid from ant d

--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AppHero from '../components/common/home/hero'
+import AppAbout from '../components/common/home/about'
 
 const AppHome = () => {
   return (
     <div className="main">
       <AppHero />
+      <AppAbout />
     </div>
   )
 }
