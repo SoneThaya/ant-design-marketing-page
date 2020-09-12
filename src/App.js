1036b9ebbc762b9d673d194e22ca8eb53b2e6816
Sone <sthaya@gmail.com>
2020-09-12 16:11:05 -0700

    styled hero with carousel

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import AppHeader from './components/common/header'
-
+import AppHome from './views/home'
 
 import { Layout } from 'antd';
 
-const { Header } = Layout;
+const { Header, Content } = Layout;
 
 function App() {
   return (
     <Layout className="mainLayout">
       <Header>
         <AppHeader />
-      
-    </Header>
-    
-  </Layout>
+      </Header>
+      <Content>
+        <AppHome />
+      </Content>
+    </Layout>
   );
 }
 
