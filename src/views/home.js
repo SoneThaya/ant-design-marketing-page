32c83b8cd36dcd4b47146a5840e65f12d7f31961
Sone <sthaya@gmail.com>
2020-09-12 17:15:32 -0700

    creatred app works component, used modal to pop up a video

--- src/views/home.js
+++ src/views/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import AppHero from '../components/common/home/hero'
 import AppAbout from '../components/common/home/about'
 import AppFeature from '../components/common/home/feature'
+import AppWorks from '../components/common/home/works'
 
 const AppHome = () => {
   return (
@@ -9,6 +10,7 @@ const AppHome = () => {
       <AppHero />
       <AppAbout />
       <AppFeature />
+      <AppWorks />
     </div>
   )
 }
