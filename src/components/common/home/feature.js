7674c6bd2d2570e07bd431143acc972e0d3a6909
Sone <sthaya@gmail.com>
2020-09-12 17:01:20 -0700

    created feature components, added card and images

---
+++ src/components/common/home/feature.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Row, Col, Card } from 'antd';
+
+import image1 from '../../../assets/images/modern-design.jpg';
+import image2 from '../../../assets/images/clean-design.jpg';
+import image3 from '../../../assets/images/great-support.jpg';
+import image4 from '../../../assets/images/easy-customise.jpg';
+import image5 from '../../../assets/images/unlimited-features.jpg';
+import image6 from '../../../assets/images/advanced-option.jpg';
+
+const { Meta } = Card;
+
+const AppFeature = () => {
+  return (
+    <div className="block featureBlock bg-gray">
+      <div className="container-fluid">
+        <Row gutter={[16, 16]}>
+          <Col span={8}>
+            <Card
+              hoverable
+              cover={<img alt="modern design" src={image1} />}
+            >
+              <Meta title="Modern Design" />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              hoverable
+              cover={<img alt="clean and elegant" src={image2} />}
+            >
+              <Meta title="Clean and Elegant" />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              hoverable
+              cover={<img alt="Advanced Options" src={image3} />}
+            >
+              <Meta title="Advanced Options" />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              hoverable
+              cover={<img alt="Great Support" src={image4} />}
+            >
+              <Meta title="Great Support" />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              hoverable
+              cover={<img alt="Easy to customize" src={image5} />}
+            >
+              <Meta title="Easy to Customize" />
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              hoverable
+              cover={<img alt="unlimited features" src={image6} />}
+            >
+              <Meta title="Unlimited Features" />
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  )
+}
+
+export default AppFeature
--- src/views/home.js
+++ src/views/home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AppHero from '../components/common/home/hero'
 import AppAbout from '../components/common/home/about'
+import AppFeature from '../components/common/home/feature'
 
 const AppHome = () => {
   return (
     <div className="main">
       <AppHero />
       <AppAbout />
+      <AppFeature />
     </div>
   )
 }
