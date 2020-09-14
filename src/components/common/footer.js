9083985bc39823d1fa876e034d45a47c3a5e69da
Sone <sthaya@gmail.com>
2020-09-13 21:07:50 -0700

    changed links

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -7,38 +7,40 @@ const AppFooter = () => {
       <div className="footer">
         <div className="logo">
           <i className="fas fa-bolt"></i>
-          <a href="#">Tech</a>
+          <a href="https://www.google.com">Tech</a>
         </div>
         <ul className="socials">
           <li>
-            <a href="#">
+            <a href="https://www.facebook.com">
               <i className="fab fa-facebook-f"></i>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="https://www.twitter.com">
               <i className="fab fa-twitter"></i>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="https://www.linkedin.com">
               <i className="fab fa-linkedin"></i>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="https://www.pinterest.com">
               <i className="fab fa-pinterest"></i>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="https://www.instagram.com">
               <i className="fab fa-instagram"></i>
             </a>
           </li>
         </ul>
         <div className="copyright">Copyright &copy; 2020 Tech</div>
         <BackTop>
-          <div className="goTop"><i className="fas fa-arrow-circle-up"></i></div>
+          <div className="goTop">
+            <i className="fas fa-arrow-circle-up"></i>
+          </div>
         </BackTop>
       </div>
     </div>
